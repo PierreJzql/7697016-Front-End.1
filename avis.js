ac66b72a9894eccd305985e1dec7f0a65c91f91f
PierreJzql <pierrejezequel92@gmail.com>
2023-03-17 15:07:59 +0100

    commit P3 finish

--- avis.js
+++ avis.js
@@ -7,31 +7,42 @@ export function ajoutListenersAvis() {
     for (let i = 0; i < piecesElements.length; i++) {
 
       piecesElements[i].addEventListener("click", async function (event) {
+      
+
+            const id = event.target.dataset.id;
+            const reponse = await fetch(`http://localhost:8081/pieces/${id}/avis`);
+            const avis = await reponse.json();
+
+
+            window.localStorage.setItem(`avis-piece-${id}`, JSON.stringify(avis))
     
-        const id = event.target.dataset.id;
-        const reponse = await fetch(`http://localhost:8081/pieces/${id}/avis`);
-        const avis = await reponse.json();
+        
         
          // Ajout de la réponse des avis
-         const pieceElement=event.target.parentElement;
-
-        const avisElement = document.createElement("p");
-
-       
-        for (let i=0; i< avis.length;i++){
-
-            avisElement.innerHTML += ` <br><b><i><u>${avis[i].utilisateur}:</u></i></b>
-            ${avis[i].commentaire} <br>`;
-            
-        }
-        
-        pieceElement.appendChild(avisElement);
-        
-        
+        const pieceElement=event.target.parentElement;
+        afficherAvis(pieceElement,avis)
       });
+      
     }
     
 }
+
+export function afficherAvis(pieceElement, avis){
+    const avisElement = document.createElement("p");
+        for (let i=0; i< avis.length;i++){
+
+            avisElement.innerHTML += ` <br><b><i><u>${avis[i].utilisateur}:</u></i></b>
+            ${avis[i].commentaire} <br>
+            Note : ${avis[i].nbEtoiles?? "Pas d'évalution"} <br>  `;
+           
+        }
+        
+        pieceElement.appendChild(avisElement);
+
+}
+
+
+
 export function ajoutListenerEnvoyerAvis(){
     // Ajout de la fonction envoyer un avis
     const formulaireAvis = document.querySelector(".formulaire-avis");
@@ -42,6 +53,7 @@ export function ajoutListenerEnvoyerAvis(){
         pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
         utilisateur: event.target.querySelector("[name=utilisateur").value,
         commentaire: event.target.querySelector("[name=commentaire]").value,
+        nbEtoiles: parseInt(event.target.querySelector("[name=notation]").value),
     };
 
     // Création de la charge utile au format JSON
--- pieces.js
+++ pieces.js
@@ -1,10 +1,26 @@
 // récupération de la liste d'avis depuis le serveur HTTP
-import { ajoutListenersAvis, ajoutListenerEnvoyerAvis } from "./avis.js";
+import { ajoutListenersAvis, ajoutListenerEnvoyerAvis, afficherAvis } from "./avis.js";
 
-// Récupération des pièces depuis le fichier JSON
-// const reponse = await fetch('pieces-autos.json');
-const reponse = await fetch(`http://localhost:8081/pieces/`);
-const pieces = await reponse.json();
+// Récupération des pièces éventuellement stockées dans le localStorage
+let pieces = window.localStorage.getItem("pieces");
+
+if (pieces === null){
+    // Récupération des pièces depuis le fichier JSON
+    // const reponse = await fetch('pieces-autos.json');
+
+    // Récupération des pièces depuis l'API
+    const reponse = await fetch(`http://localhost:8081/pieces/`);
+    pieces = await reponse.json();
+
+    // Transformation des pièces en JSON
+    const valeurPieces = JSON.stringify(pieces);
+    
+    // Stockage des informations dans le localStorage
+    window.localStorage.setItem("pieces", valeurPieces);
+}else{
+    pieces = JSON.parse(pieces);
+}
+
 
 // On appel la fonction pour ajouter le listener au formulaire
 ajoutListenerEnvoyerAvis()
@@ -57,6 +73,17 @@ function genererPieces(pieces){
     ajoutListenersAvis();
 }
 genererPieces(pieces);
+
+for(let i =0; i<pieces.length;i++){
+    const id = pieces[i].id;
+    const avisJSON = window.localStorage.getItem(`avis-piece-${id}`);
+    const avis = JSON.parse(avisJSON);
+
+    if (avis !== null){
+        const pieceElement = document.querySelector(`article[data-id="${id}"]`);
+        afficherAvis(pieceElement,avis)
+    }
+}
 
 
 // Gestion des boutons
@@ -165,4 +192,10 @@ inputPrixMax.addEventListener('input', function (){
     });
     document.querySelector(".fiches").innerHTML = "";
     genererPieces(piecesFiltrees);
+});
+
+// Ajout du listener pour mettre à jour des données du localStorage
+const boutonMettreAJour = document.querySelector(".btn-maj");
+boutonMettreAJour.addEventListener("click", function () {
+  window.localStorage.removeItem("pieces");
 });
